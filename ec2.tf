316ac230ae643076c7e8ad0df33ab99cd4e0081a
Naga <pavantonystark99@gmail.com>
2025-10-14 16:38:39 +0530

    added ami

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "terraforma" {
-  ami           = ""
+  ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
+    Terraform = "true"
   }
 }
